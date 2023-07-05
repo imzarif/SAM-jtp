f087264e4ce900d267b1b70b77f1e80409be64ce
Zarif Ahmed <imzahmed116@gmail.com>
2023-07-05 10:29:36 +0600

    Commentred some decoder code

--- segment_anything/modeling/mask_decoder.py
+++ segment_anything/modeling/mask_decoder.py
@@ -13,6 +13,7 @@ from typing import List, Tuple, Type
 from .common import LayerNorm2d
 
 
+
 class MaskDecoder(nn.Module):
     def __init__(
         self,
@@ -120,13 +121,18 @@ class MaskDecoder(nn.Module):
         # Concatenate output tokens
         print("runn nnnnnnn")
         output_tokens = torch.cat([self.iou_token.weight, self.mask_tokens.weight], dim=0)
+         print("Output Tokens 1: " + output_tokens.shape)
+          print("Sparse prompt embedding 1: "+ sparse_prompt_embeddings.shape)
         output_tokens = output_tokens.unsqueeze(0).expand(sparse_prompt_embeddings.size(0), -1, -1)
 
         # Amader code because dim of output_token=3 and prompt dim=2    
-        sparse_prompt_embeddings = sparse_prompt_embeddings[:, :256]  # Keep the first 256 elements along dimension 1
-        sparse_prompt_embeddings = sparse_prompt_embeddings.unsqueeze(0).expand(output_tokens.size(0), -1, -1)  # Expand to match the size of output_tokens
+        # sparse_prompt_embeddings = sparse_prompt_embeddings[:, :256]  # Keep the first 256 elements along dimension 1
+        # sparse_prompt_embeddings = sparse_prompt_embeddings.unsqueeze(0).expand(output_tokens.size(0), -1, -1)  # Expand to match the size of output_tokens
 
         tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=1)
+
+        print("Output Tokens 2: " + output_tokens.shape)
+        print("Sparse prompt embedding 2: "+ sparse_prompt_embeddings.shape)
 
         # Expand per-image data in batch direction to be per-mask
         src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
